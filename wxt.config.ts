addd12f5aa5e4b454056bebc843cbcf78d71031c
Justin Bennett <git@just-be.dev>
2024-12-21 20:59:50 -0500

    Add MeltUI; Tailwind
    Melt has a good tree component which is the primary thing I need. I'm using the newest version of Tailwind which still isn't RC

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  extensionApi: 'webextension-polyfill',
-  modules: ['@wxt-dev/module-svelte'],
+  srcDir: "src",
+  extensionApi: "webextension-polyfill",
+  modules: ["@wxt-dev/module-svelte"],
+  vite: () => ({
+    plugins: [
+      // TODO there's a type conflict here
+      tailwindcss() as any,
+    ],
+  }),
+  svelte: {
+    vite: {
+      preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+    },
+  },
 });
