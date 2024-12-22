ffa3b5719d97ea11b92c98799e9a3412681b41c4
Justin Bennett <git@just-be.dev>
2024-12-21 19:39:50 -0500

    Use webextension polyfill with proper types
    Getting the types to work for the `Browser` namespace was incredibly annoying.

--- wxt-env.d.ts
+++ wxt-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="svelte" />
+import type Polyfill from 'webextension-polyfill';
+
+declare global {
+  export type * as Browser from 'webextension-polyfill';
+  const browser: Polyfill;
+}
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  extensionApi: 'chrome',
+  extensionApi: 'webextension-polyfill',
   modules: ['@wxt-dev/module-svelte'],
 });
