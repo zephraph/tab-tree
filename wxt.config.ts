84e856bd0a273839728bdc68387bf0b96535c8c5
Justin Bennett <git@just-be.dev>
2024-12-21 21:25:48 -0500

    Support $lib
    Ensures vite can find components in $lib

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "wxt";
 import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   extensionApi: "webextension-polyfill",
   modules: ["@wxt-dev/module-svelte"],
   vite: () => ({
+    resolve: {
+      alias: {
+        $lib: path.resolve(__dirname, "./src/lib"),
+      },
+    },
     plugins: [
       // TODO there's a type conflict here
       tailwindcss() as any,
